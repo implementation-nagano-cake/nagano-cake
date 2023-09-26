81b295ec2381b77821a4f6b1cef970adbcde4205
EC2 Default User <141564706+Satoshi-1217@users.noreply.github.com>
2023-09-26 17:55:49 +0000

    [update]controller, view, routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
   
-  root :to => 'homes#top'
+  root :to => 'public/homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
   resources :items, only: [:index, :show]
   resources :cart_items, only: [:index, :create, :update, :destroy]
